4465a38c41f6c0578182699facd5df90f39bbe3f
Tatar73 <119617920+Tatar73@users.noreply.github.com>
2023-01-17 14:29:45 +0100

    page contact static

--- templates/page-animation.php
+++ templates/page-animation.php
@@ -26,23 +26,28 @@ $slider = get_field('slider');
         <img src="<?php echo (get_template_directory_uri()); ?>/assets/img/icon/lune.svg" alt="illustration">
     </div>
     <h2>PROGRAMME</h2>
+</section>
+<section class="programme">
+    <div class="ligne"></div>
     <div class="animation-slider">
-        <div class="main-carousel" data-flickity='{ "cellAlign": "right", "contain": true, "wrapAround": true }'>
+        <div class="main-carousel" data-flickity='{ "cellAlign": "center", "contain": true, "wrapAround": true }'>
             <?php foreach ($slider['images'] as $image): ?>
-                <div id="horizontal_img" class="carousel-cell">
-                    <p><?php echo ($image['categorie']); ?></p>
+                <div class="carousel-cell">
+                    <img src="<?php echo (get_template_directory_uri()); ?>/assets/img/icon/selecteur.svg" alt="selecteur">
+                    <p class="categorie"><?php echo ($image['categorie']); ?></p>
                     <img src="<?php echo ($image['image']['url']); ?>" height="" width="" alt="">
-                    <p><?php echo ($image['titre']); ?></p>
-                    <p><?php echo ($image['text']); ?></p>
+                    <p class="titre"><?php echo ($image['title']); ?></p>
+                    <p class="text"><?php echo ($image['text']); ?></p>
                 </div>
             <?php endforeach; ?>
         </div>
     </div>
-
-
-
 </section>
 
+
+
+<script type="text/javascript" src="<?php echo (get_template_directory_uri()); ?>/assets/js/libs/flickity.js"
+    async></script>
 <?php
 //Footer
 get_footer();
--- templates/page-contact.php
+++ templates/page-contact.php
@@ -5,11 +5,19 @@
 // Header
 get_header();
 ?>
-<section>
+<section class="contact">
     <h1>Contact</h1>
     <h3>Pour plus d’informations, n’hésitez pas à nous contacter</h3>
-    <h2>UNE QUESTION ?</h2>
-    <form action="" [contact-form-7 id="89" title="Formulaire de contact 1" ]>
-    </form>
+    <h2>
+        <?php the_field('title') ?>
+    </h2>
+    <div class="form">
+        <div class="left">
+            <?php echo do_shortcode('[wpforms id="147" title="false"]'); ?>
+        </div>
+        <div class="right">
+            <img src="<?php echo (get_template_directory_uri()); ?>/assets/img/icon/mail.svg" alt="illustration mail">
+        </div>
+    </div>
 </section>
 <?php get_footer() ?>
