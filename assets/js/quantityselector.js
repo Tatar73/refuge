b9cc4bc6ca1ac66028380975345c71ae3c61cd9a
Tatar73 <119617920+Tatar73@users.noreply.github.com>
2023-01-19 09:51:29 +0100

    tarifs form quantity fixed

--- assets/js/quantityselector.js
+++ assets/js/quantityselector.js
@@ -1,20 +1,27 @@
-let quantitesEl = document.querySelectorAll(".quantite");
+(function () {
+const quantitesEl = document.querySelectorAll(".quantite");
 
 
 quantitesEl.forEach(function(el){
-    let inputEl = el.querySelector('input');
+    const inputEl = el.querySelector('input');
     let quantity = parseInt(inputEl.value, 10);
     console.log('quantity', quantity);
-    let button_moins = el.querySelector('.moins');
-    let button_plus = el.querySelector('.plus');
+    const button_moins = el.querySelector('.moins');
+    const button_plus = el.querySelector('.plus');
 
     button_moins.addEventListener('click', function(){
+        evt.preventDefault();
+        evt.stopPropagation();
         quantity -= 1;
         inputEl.value = quantity;
     })
     button_plus.addEventListener('click', function(){
+        evt.preventDefault();
+        evt.stopPropagation();
         quantity += 1;
         inputEl.value = quantity;
     })
 
 })
+
+})()
