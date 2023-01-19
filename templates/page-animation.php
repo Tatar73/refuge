34bf0debb0129e9b4f96678910772ff5da456031
Tatar73 <119617920+Tatar73@users.noreply.github.com>
2023-01-19 10:40:50 +0100

    flickity slider fixed

--- templates/page-animation.php
+++ templates/page-animation.php
@@ -27,6 +27,15 @@ $slider = get_field('slider');
     <div class="ligne"></div>
     <div class="animation-slider">
         <div class="main-carousel" data-flickity='{ "cellAlign": "center", "contain": true, "wrapAround": true }'>
+        <?php foreach ($slider['images'] as $image): ?>
+                <div class="carousel-cell">
+                    <img src="<?php echo (get_template_directory_uri()); ?>/assets/img/icon/selecteur.svg" alt="selecteur">
+                    <p class="categorie"><?php echo ($image['categorie']); ?></p>
+                    <img src="<?php echo ($image['image']['url']); ?>" height="" width="" alt="">
+                    <p class="titre"><?php echo ($image['title']); ?></p>
+                    <p class="text"><?php echo ($image['text']); ?></p>
+                </div>
+            <?php endforeach; ?>
             <?php foreach ($slider['images'] as $image): ?>
                 <div class="carousel-cell">
                     <img src="<?php echo (get_template_directory_uri()); ?>/assets/img/icon/selecteur.svg" alt="selecteur">
