1639ebcce22cc2dcf3001dc976e4463db69dc3ba
Tatar73 <119617920+Tatar73@users.noreply.github.com>
2023-01-20 01:17:35 +0100

    animation + index responsive

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
                 <img id="logo_refuge" src="<?php echo (get_template_directory_uri()); ?>/assets/img/logo_refuge.svg"
                     alt="">
             </a>
-
+            
             <ul>
                 <li><a href="acces" <?php if (is_page()) {
                     echo 'class="active"';
