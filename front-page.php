97cf39a1285c26066d20dfbb6414ff74e723d726
Tatar73 <119617920+Tatar73@users.noreply.github.com>
2023-01-24 21:34:10 +0100

    favicon

--- front-page.php
+++ front-page.php
@@ -292,8 +292,8 @@ $slider = get_field('slider');
 
 <script type="text/javascript" src="<?php echo (get_template_directory_uri()); ?>/assets/js/libs/flickity.js"
     async></script>
+    
 <?php
-//Footer
+// Footer
 get_footer();
-
 ?>
