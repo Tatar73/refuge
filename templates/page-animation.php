2ba92e9db50abe5953e27f440e7951200b49b470
Tatar73 <119617920+Tatar73@users.noreply.github.com>
2023-01-25 00:27:51 +0100

    animation date evenements

--- templates/page-animation.php
+++ templates/page-animation.php
@@ -21,28 +21,50 @@ $slider = get_field('slider');
         </p>
         <img src="<?php echo (get_template_directory_uri()); ?>/assets/img/icon/lune.svg" alt="illustration">
     </div>
-    <h2><?php the_field('titre2'); ?></h2>
+    <h2>
+        <?php the_field('titre2'); ?>
+    </h2>
 </section>
 <section class="programme">
     <div class="ligne"></div>
     <div class="animation-slider">
         <div class="main-carousel" data-flickity='{ "cellAlign": "center", "contain": true, "wrapAround": true }'>
-        <?php foreach ($slider['images'] as $image): ?>
+            <?php foreach ($slider['images'] as $image): ?>
                 <div class="carousel-cell">
-                    <img src="<?php echo (get_template_directory_uri()); ?>/assets/img/icon/selecteur.svg" alt="selecteur">
-                    <p class="categorie"><?php echo ($image['categorie']); ?></p>
-                    <img src="<?php echo ($image['image']['url']); ?>" height="" width="" alt="">
-                    <p class="titre"><?php echo ($image['title']); ?></p>
-                    <p class="text"><?php echo ($image['text']); ?></p>
+                    <p class="date">
+                        <?php echo ($image['date']); ?>
+                    </p>
+                    <img class="selecteur" src="<?php echo (get_template_directory_uri()); ?>/assets/img/icon/selecteur.svg"
+                        alt="selecteur">
+                    <p class="categorie">
+                        <?php echo ($image['categorie']); ?>
+                    </p>
+                    <img class="illustration" src="<?php echo ($image['image']['url']); ?>" height="" width="" alt="">
+                    <p class="titre">
+                        <?php echo ($image['title']); ?>
+                    </p>
+                    <p class="text">
+                        <?php echo ($image['text']); ?>
+                    </p>
                 </div>
             <?php endforeach; ?>
             <?php foreach ($slider['images'] as $image): ?>
                 <div class="carousel-cell">
-                    <img src="<?php echo (get_template_directory_uri()); ?>/assets/img/icon/selecteur.svg" alt="selecteur">
-                    <p class="categorie"><?php echo ($image['categorie']); ?></p>
-                    <img src="<?php echo ($image['image']['url']); ?>" height="" width="" alt="">
-                    <p class="titre"><?php echo ($image['title']); ?></p>
-                    <p class="text"><?php echo ($image['text']); ?></p>
+                    <p class="date">
+                        <?php echo ($image['date']); ?>
+                    </p>
+                    <img class="selecteur" src="<?php echo (get_template_directory_uri()); ?>/assets/img/icon/selecteur.svg"
+                        alt="selecteur">
+                    <p class="categorie">
+                        <?php echo ($image['categorie']); ?>
+                    </p>
+                    <img class="illustration" src="<?php echo ($image['image']['url']); ?>" height="" width="" alt="">
+                    <p class="titre">
+                        <?php echo ($image['title']); ?>
+                    </p>
+                    <p class="text">
+                        <?php echo ($image['text']); ?>
+                    </p>
                 </div>
             <?php endforeach; ?>
         </div>
