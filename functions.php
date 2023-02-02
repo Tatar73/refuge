684a221eeceef2e940f726b9dc7f63e55f78b7bd
Tatar73 <119617920+Tatar73@users.noreply.github.com>
2023-02-02 10:20:47 +0100

    test pop up tarifs

--- functions.php
+++ functions.php
@@ -2,7 +2,6 @@
 if (!is_admin()) {
      wp_enqueue_style('app', get_template_directory_uri() . '/assets/css/app.css', $deps = array(), 1, 'all');
      wp_enqueue_script('app-js', get_template_directory_uri() . '/assets/js/quantityselector.js', $deps = array(), 1, 'all');
-
 }
 
 function debug($vars)
